f5471ec24823335f24e4a492f63c7a913c685539
Ian Davies <github@iandavies.org>
2014-07-04 14:50:20 +0100

    Running robot/refuel.py

--- robot/refuel.py
+++ robot/refuel.py
@@ -2,11 +2,11 @@ from tealight.robot import (move, turn, look, touch, smell, leftSide, rightSide)
 
 # Add your code here!
 while True:
-  while touch()=="wall":
-    turn(1)
-    move()
   while leftSide()=="wall":
     move()
+    while touch()=="wall":
+      turn(1)
+      move()
   turn(-1)
   move()
   print touch()
