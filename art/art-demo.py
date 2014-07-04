7bc5d44b5ffade8729b122ed5b8b92b541b5c9c5
Ian Davies <github@iandavies.org>
2014-07-04 14:48:00 +0100

    Running art/art-demo.py

--- art/art-demo.py
+++ art/art-demo.py
@@ -2,39 +2,11 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 
 from tealight.art import (screenWidth, screenHeight)
 
-print "This is art mode!"
 
-print screenWidth
-print screenHeight
+def expt():
+  heads = 0
+  for i in range(0,50,1):
+    if random() > 0.5:
+      heads = heads + 1
 
-background("paper.jpg")
-
-line(0,0,screenWidth, screenHeight)
-
-spot(200,300, 20)
-
-circle(300,200, 20)
-
-box(500, 500, 60, 60)
-
-image(200,200,"bird.png")
-
-line(560,0,560,495)
-
-text(600, 100, "Hello Tealight!")
-
-lastx = None
-lasty = None
-hue = 0
-
-def handle_mousemove(x,y):
-  global lastx, lasty, hue
-  
-  line(lastx or x, lasty or y, x, y)
-  color("hsl(%d,100%%,50%%)" % hue)
-  
-  hue += 1
-  
-  lastx = x
-  lasty = y
-  
+print expt()
