ecff2d35621b75b2221b237eefdc5a3c29fa37c0
Ian Davies <github@iandavies.org>
2014-07-04 15:10:53 +0100

    Running robot/refuel.py

--- robot/refuel.py
+++ robot/refuel.py
@@ -3,7 +3,6 @@ from tealight.robot import (move, turn, look, touch, smell, leftSide, rightSide)
 # Add your code here!
 while True:
   while touch()=="None":
-    turn(-1)
     move()
   while look()=="fruit":
     move()
