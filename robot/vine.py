1db71c9a8013268948eb03c99aacc10d19fbb6a8
Ian Davies <github@iandavies.org>
2014-07-05 21:58:20 +0100

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -9,10 +9,10 @@ def go():
     moved = moved + 1
     
   turn(-1)
-  if leftSide() == "fruit"
+  if leftSide() == "fruit":
     go()
   turn(2)
-  if rightSide() == "fruit"
+  if rightSide() == "fruit":
     go()
   
   turn(1)
