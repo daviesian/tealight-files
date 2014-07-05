5181b68e8c75adff62ffade9dff7cced12b71562
Ian Davies <github@iandavies.org>
2014-07-05 21:56:58 +0100

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -2,9 +2,22 @@ from tealight.robot import (move, turn, look, touch, smell, leftSide, rightSide)
 
 # Add your code here!
 
-def go():  
+def go(): 
+  moved = 0
   while touch() == "fruit":
     move()
+    moved = moved + 1
+    
+  turn(-1)
+  go()
+  turn(2)
+  go()
+  
+  turn(1)
+  for i in range(0,moved):
+    move()
+  turn(2)
+  
     
     
 go()
