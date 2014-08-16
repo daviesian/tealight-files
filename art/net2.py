a1af4f89b2cab5995a54e604959e31fa11372f33
Ian Davies <github@iandavies.org>
2014-08-16 23:23:51 +0100

    Running art/net2.py

--- art/net2.py
+++ art/net2.py
@@ -2,6 +2,5 @@ from tealight.utils import github_load
 
 github_load("daviesian", "art/network_test.py", "nt")
 
-import nt
-
+from nt import handle_mousemove, handle_message
 #nt.handle_mousemove(3,4)
