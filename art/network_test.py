8ab9864c31ddbc7e9932f114403f4c6553ac4ac7
Ian Davies <github@iandavies.org>
2014-08-16 17:04:39 +0100

    Running art/network_test.py

--- art/network_test.py
+++ art/network_test.py
@@ -1,1 +1,13 @@
-print "A new file!"
+from tealight.net import connect, send
+
+def handle_connected():
+  print "Connected"
+  
+def handle_message(message):
+  print "Message: %s" % message
+
+def handle_mousemove(x,y):
+  send({"x":x, "y":y})
+
+connect("net_test")
+
