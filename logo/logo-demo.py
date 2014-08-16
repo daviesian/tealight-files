75d454e5e1240d5755c114407d2853b32f42f010
Ian Davies <github@iandavies.org>
2014-08-16 14:14:31 +0100

    Running logo/logo-demo.py

--- logo/logo-demo.py
+++ logo/logo-demo.py
@@ -2,7 +2,6 @@ from tealight.logo import (move, turn,
                            pen_down, pen_up,
                            show_turtle, hide_turtle,
                            color, speed)
-eval( "print \"hi\"")
 
 print "This is logo mode!"
 
