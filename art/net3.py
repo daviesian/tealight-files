383b319ab82589af7d86cc3a325f3891a0749b04
Ian Davies <github@iandavies.org>
2014-08-17 11:10:27 +0100

    Running art/net3.py

--- art/net3.py
+++ art/net3.py
@@ -1,1 +1,11 @@
-print "A new file!"
+from tealight.art import line
+
+pos = {
+       "x": 0,
+       "y": 0
+      }
+
+def handle_mousemove(x,y):
+  line(pos["x"], pos["y"], x,y)
+  pos["x"] = x
+  pos["y"] = y
