b8bb58aabc7c51359d74798c168e208bfa412628
Ian Davies <github@iandavies.org>
2014-08-18 13:32:42 +0100

    Running art/shapes.py

--- art/shapes.py
+++ art/shapes.py
@@ -1,1 +1,7 @@
-print "A new file!"
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+
+color("blue")
+
+spot(100,200,50)
+circle(300,200,50)
+box(450,150,100,100)
