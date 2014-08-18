9aab79ff4f156f53ec665945784bcdf8d23f1f69
Ian Davies <github@iandavies.org>
2014-08-18 12:31:58 +0100

    Running logo/logo-demo.py

--- logo/logo-demo.py
+++ logo/logo-demo.py
@@ -1,29 +1,9 @@
-from tealight.logo import (move, turn,
-                           pen_down, pen_up,
-                           show_turtle, hide_turtle,
-                           color, speed)
+from tealight.logo import (move, 
+                           turn,
+                           color)
 
 
-from github.daviesian.logo.lib import lib_func
-
-import tealight.net
-
-tealight.net.connect("my_app")
-
-def handle_connected():
-  print "Connected!"
-  tealight.net.send("hello!")
-
-def handle_message(message):
-  print "Message!"
-  print message
-
-print lib_func()
-print "This is logo mode!"
-
-print 5/2
 colors = ["red", "blue", "green"]
-
 
 for i in range(10,200,5):
   move(i)
