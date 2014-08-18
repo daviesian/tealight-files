6322b622fcad44fb7ff6b5020afe2f6f4885b06e
Ian Davies <github@iandavies.org>
2014-08-18 13:58:23 +0100

    Running art/orbits.py

--- art/orbits.py
+++ art/orbits.py
@@ -7,3 +7,40 @@ vy = 0
 ax = 0
 ay = 0
 
+power = 0.3
+
+def handle_keydown(keyval):
+  global ax, ay
+  
+  if keyval == 37: # Left
+    ax = -power
+  elif keyval = 39: # Right
+    ax = power
+  elif keyval = 38: # Up
+    ay = -power
+  elif keyval = 40: # Down
+    ay = power
+
+def handle_keyup(keyval):
+  global ax, ay
+  
+  if keyval == 37 or keyval == 39:
+    ax = 0
+  elif keyval == 38 or keyval == 40:
+    ay = 0
+    
+def handle_frame():
+  color("white")
+  
+  spot(x,y,8)
+  vx = vx + ax
+  vy = vy + ay
+  
+  x = x + vx
+  y = y + vy
+  
+  color("blue")
+  
+  spot(x,y,8)
+  
+  
