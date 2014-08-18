4e691b066714bd02554d2bdfa539285e4c93f626
Ian Davies <github@iandavies.org>
2014-08-18 13:11:26 +0100

    Closing robot/refuel.py

--- robot/refuel.py
+++ robot/refuel.py
@@ -1,19 +1,9 @@
-from tealight.robot import (move, turn, look, touch, smell, leftSide, rightSide)
+from tealight.robot import (move, 
+                            turn, 
+                            look, 
+                            touch, 
+                            smell, 
+                            left_side, 
+                            right_side)
 
-# Add your code here!
-while True:
-  while touch()=="None":
-    move()
-  while look()=="fruit":
-    move()
-  while leftSide()=="wall":
-      move()
-      while touch()=="wall":
-        turn(1)
-        move()
-  turn(-1)
-  move()
-  print look()
-  print touch()
-  
-  
+# This is a fairly useless algorithm!
