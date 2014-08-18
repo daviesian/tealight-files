2d66e639b59915b09ff50165aa534173360e6934
Ian Davies <github@iandavies.org>
2014-08-18 10:47:43 +0100

    Running art/keyboard_test.py

--- art/keyboard_test.py
+++ art/keyboard_test.py
@@ -1,1 +1,8 @@
-print "A new file!"
+from tealight.art import line, spot, screen_width, screen_height
+
+x = screen_width / 2
+y = screen_height / 2
+
+def handle_keydown(keychar, keyval):
+  print keychar + keyval
+
