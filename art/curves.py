48ae8c244c1cc3d7dc8d384ff8808834046b75c4
Ian Davies <github@iandavies.org>
2014-08-18 13:50:02 +0100

    Running art/curves.py

--- art/curves.py
+++ art/curves.py
@@ -1,7 +1,7 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
-for x in range(54):
-  for y in range(39):
+for x in range(540):
+  for y in range(390):
     if y > x*x:
       color("red")
     elif y > x:
