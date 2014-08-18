2d6116e0d1ef86b2b5f723c20b6d481c9f65ca93
Ian Davies <github@iandavies.org>
2014-08-18 09:20:43 +0100

    Running art/net2.py

--- art/net2.py
+++ art/net2.py
@@ -1,6 +1,3 @@
-from tealight.utils import github_load
+from math import degrees, radians, pi
 
-github_load("daviesian", "art/network_test.py", "nt")
-
-from nt import handle_mousemove, handle_message
-#nt.handle_mousemove(3,4)
+print degrees(pi)
