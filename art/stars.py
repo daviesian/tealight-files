178b18c83ef103e2658ef7bc6a22e50b72f3ab56
Ian Davies <github@iandavies.org>
2014-08-18 13:37:47 +0100

    Running art/stars.py

--- art/stars.py
+++ art/stars.py
@@ -1,1 +1,17 @@
-print "A new file!"
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+
+from math import sin, cos, pi
+
+def star(x, y, c, size, spines):
+  
+  color(c)
+  
+  angle = 0
+  
+  for i in range(0, spines):
+    x0 = x + (size * cos(angle))
+    y0 = y + (size * sin(angle))
+    
+    line(x, y, x0, y0)
+    
+    angle = angle + (2 * pi / spines)
