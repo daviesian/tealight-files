d874be36e52d20dbd516b91c24bab87abbe7bfff
Ian Davies <github@iandavies.org>
2014-08-18 13:19:55 +0100

    Running art/input.py

--- art/input.py
+++ art/input.py
@@ -2,3 +2,7 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 
 def handle_mousedown(x,y):
   spot(x,y,10)
+  
+def handle_mousemove(x,y,button):
+  if button == "left":
+    spot(x,y,10)
