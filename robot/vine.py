b0360fc52a318537e544a17754a6deedfc6326b5
Ian Davies <github@iandavies.org>
2014-08-18 13:12:16 +0100

    Closing robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -1,23 +1,9 @@
 from tealight.robot import (move, 
                             turn, 
-                            touch)
+                            look, 
+                            touch, 
+                            smell, 
+                            left_side, 
+                            right_side)
 
-def go(): 
-  moved = 0
-  while touch() == "fruit":
-    move()
-    moved = moved + 1
-    
-  turn(-1)
-  if touch() == "fruit":
-    go()
-  turn(2)
-  if touch() == "fruit":
-    go()
-  
-  turn(1)
-  for i in range(0,moved):
-    move()
-  turn(2)
-    
-go()
+# Add your code here
