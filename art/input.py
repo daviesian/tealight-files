36e1ed82c8f9c46add4a25bee282be4ac893ee94
Ian Davies <github@iandavies.org>
2014-08-18 13:27:35 +0100

    Running art/input.py

--- art/input.py
+++ art/input.py
@@ -4,6 +4,5 @@ def handle_mousedown(x,y):
   spot(x,y,10)
   
 def handle_mousemove(x,y,button):
-  print button=="left"
-  #if button == "left":
-  #  spot(x,y,10)
+  if button == "left":
+    spot(x,y,10)
