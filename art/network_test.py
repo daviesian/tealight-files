042faa19a6d212c8787b7eceefab91571eb3fa5a
Ian Davies <github@iandavies.org>
2014-08-19 17:22:18 +0100

    Running art/network_test.py

--- art/network_test.py
+++ art/network_test.py
@@ -9,7 +9,7 @@ def handle_message(message):
   print message["x"]
 
 def handle_mousemove(x,y):
-  send({"x":x, "y":y},8,9)
+  send({"x":x, "y":y},8,9,)
 
 connect("net_test")
 send({"x":1, "y":2}, True)
