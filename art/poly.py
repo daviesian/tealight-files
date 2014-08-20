8851e58c95c55d6d9281c88b4cde9aea8d9f5fd1
Ian Davies <github@iandavies.org>
2014-08-20 13:58:01 +0100

    Running art/poly.py

--- art/poly.py
+++ art/poly.py
@@ -3,4 +3,4 @@ from tealight.art import polygon, fill_polygon, test_polygon
 c = [(100,100), (100,200), (200,300), (200, 50)]
 fill_polygon(c)
 
-print False#test_polygon(10,10,c)
+print test_polygon(10,10,c)
