9a555866a207ff654f361b7461644e8e7338ec99
Ian Davies <github@iandavies.org>
2014-08-20 14:53:29 +0100

    Running art/poly.py

--- art/poly.py
+++ art/poly.py
@@ -3,7 +3,7 @@ from tealight.art import *
 c = [(100,100), (50,200), (200,300), (270, 50), (270, 400), (290,20)]
 fill_polygon(c)
 
-#color("red")
-#line(120,150,1000,150)
+color("red")
+line(50,200,1000,200)
 #spot(283,150,5)
 print test_polygon(50,200,c)
