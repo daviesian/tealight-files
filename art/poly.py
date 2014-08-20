8b8884f8615c4d20878bb16153041405aa3f9d02
Ian Davies <github@iandavies.org>
2014-08-20 14:45:36 +0100

    Running art/poly.py

--- art/poly.py
+++ art/poly.py
@@ -1,6 +1,8 @@
-from tealight.art import polygon, fill_polygon, test_polygon
+from tealight.art import line, polygon, fill_polygon, test_polygon
 
 c = [(100,100), (50,200), (200,300), (200, 50)]
 fill_polygon(c)
 
+color("red")
+line(120,150,0,150)
 print test_polygon(120,150,c)
