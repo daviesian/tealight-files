1ad9513ff4d6e9d178c56e5d6409a5c9c8c22978
Ian Davies <github@iandavies.org>
2014-08-20 14:54:36 +0100

    Running art/poly.py

--- art/poly.py
+++ art/poly.py
@@ -7,3 +7,10 @@ color("red")
 line(50,200,1000,200)
 #spot(283,150,5)
 print test_polygon(49,200,c)
+
+def handle_mousedown(x,y):
+  if test_polygon(x,y,c):
+    color("red")
+  else:
+    color("blue")
+  circle(x,y,10)
