a43336dafffd10ace3517d735ab9249e6aa66de3
Ian Davies <github@iandavies.org>
2014-08-20 14:59:45 +0100

    Running art/poly.py

--- art/poly.py
+++ art/poly.py
@@ -10,8 +10,8 @@ print test_polygon(49,200,c)
 
 
 
-for i in range(0,500,2):
-  for j in range(0,500,2):
+for i in range(0,500,4):
+  for j in range(0,500,4):
     if test_polygon(i,j,c):
       color("red")
     else:
