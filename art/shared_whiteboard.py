35c108e9ba31469b6d1569eb08a255fe898137d2
Ian Davies <github@iandavies.org>
2014-08-20 16:45:58 +0100

    Running art/shared_whiteboard.py

--- art/shared_whiteboard.py
+++ art/shared_whiteboard.py
@@ -2,10 +2,6 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 
 from tealight.net import connect, send
 
-from github.daviesian.logo.lib import lib_func
-
-print lib_func()
-
 lastx = 0
 lasty = 0
 
