1e68f600606d49bb2bb070b1a44f389bc6e0613c
Ian Davies <github@iandavies.org>
2014-08-21 17:20:34 +0100

    Running art/triangle.py

--- art/triangle.py
+++ art/triangle.py
@@ -32,7 +32,7 @@ def draw():
   
   for i in range(0, num_triangles):
     size = min_size + (max_size - min_size) * float(i) / num_triangles
-    size *= (sin(age)*0.1 + 1)
+    size *= (sin(age())*0.1 + 1)
     
     l = min_l + (max_l - min_l) * float(i) / num_triangles
     
