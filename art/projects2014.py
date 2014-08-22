301f5b9aa4288e3460df247705f94232355103ba
Ian Davies <github@iandavies.org>
2014-08-22 11:57:35 +0100

    Running art/projects2014.py

--- art/projects2014.py
+++ art/projects2014.py
@@ -8,4 +8,7 @@
 #from github.griffithsben.art.Connect4Main import *
 
 ## GROUP 4
-from github.anthonyajsmith.art.othello import *
+#from github.anthonyajsmith.art.othello import *
+
+## GROUP 5
+from github.davidsamueljones.art.Minesweeper import *
