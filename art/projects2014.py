532ad7e1d162dd278c2afef5d80c73235c69fff4
Ian Davies <github@iandavies.org>
2014-08-22 15:24:30 +0100

    Running art/projects2014.py

--- art/projects2014.py
+++ art/projects2014.py
@@ -2,13 +2,13 @@
 #from github.chandler6.art.connectfourmain import *
 
 ## GROUP 2
-#from github.shivam1023.art.MemoryGame import *
+from github.shivam1023.art.MemoryGame import *
 
 ## GROUP 3
 #from github.griffithsben.art.Connect4Main import *
 
 ## GROUP 4
-from github.anthonyajsmith.art.othello import *
+#from github.anthonyajsmith.art.othello import *
 
 ## GROUP 5
 #from github.davidsamueljones.art.Minesweeper import *
