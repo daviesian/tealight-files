19086a9f2eff2ad3029ae6d687f9b217d650078e
Ian Davies <github@iandavies.org>
2014-09-07 10:17:34 +0100

    Running art/art-demo.py

--- art/art-demo.py
+++ art/art-demo.py
@@ -4,12 +4,6 @@ from tealight.art import (screen_width, screen_height)
 
 from random import random
 
-def handle_frame():
-  background("paper.jpg")
-  for i in range(0,100):
-    image(0,i,"bird.png")
-    
-    
     
 spot(8,4,5)
 def expt():
